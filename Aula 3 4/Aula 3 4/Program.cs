52469df88bd28b5ad982a63e2b41d53aa0015f2c
gvsg09 <joaovsguerreiro@gmail.com>
2022-02-23 16:57:29 +0000

    Exercicio 2

--- Aula 3 4/Aula 3 4/Program.cs
+++ Aula 3 4/Aula 3 4/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Bem-vindos ao C#");
+
+//Exercicio 2 
+string nome = "alunos";
+string msg = "Bem-vindos";
+//1
+Console.WriteLine("Caros {0} sejam {1} ao C#!",nome, msg);
+
+//2
+Console.WriteLine("Caros " + nome + " sejam " + msg + " ao C#!");
+
+
+
+

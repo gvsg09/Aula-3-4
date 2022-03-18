05b5d9417bcf9778141b92517237de4c338c9103
gvsg09 <joaovsguerreiro@gmail.com>
2022-03-18 16:58:29 +0000

    Aula 6 7

--- Aula 3 4/Aula 3 4/Program.cs
+++ Aula 3 4/Aula 3 4/Program.cs
@@ -55,6 +55,151 @@ double z = Math.Ceiling(4.1);
 Console.WriteLine(z);
 double t =Math.Round(Math.Sin(30*Math.PI/180),2);
 Console.WriteLine(t);
+Console.WriteLine("");
+
+//Exercício 8
+
+bool a = 4 == 5;
+Console.WriteLine(a);
+bool b = 4 != 6;
+Console.WriteLine(b);
+bool c = 4 > 5;
+Console.WriteLine(c);
+bool d = 4 < 5 && 6 < 10;
+Console.WriteLine(d);
+bool e = 40 < 50 || 60 > 90;
+Console.WriteLine(e);
+bool f = !(40 < 50 || 60 > 90);
+Console.WriteLine(f);
+Console.WriteLine("");
+
+//Exercicio 9
+
+string str = "Bom dia,";
+string str1 = " tudo bem?";
+Console.WriteLine("O comprimento da string '{0}' é {1}", str, str.Length);
+Console.WriteLine(str + str1);
+Console.WriteLine(str.ToUpper());
+Console.WriteLine(str.Substring(3));
+Console.WriteLine(str.IndexOf("dia"));
+Console.WriteLine(str.CompareTo(str1));
+Console.WriteLine(str1.Trim());
+Console.WriteLine("");
+
+//Exercicio 10 
+
+sbyte x1 = 34;
+byte y1 = 250;
+short z1 = 32000;
+ushort var1 = 60000;
+int var2 = 1000000012;
+uint var3 = 3500000000;
+long var4 = 1532333333334581239;
+ulong var5 = 14532333333334581239;
+
+Console.WriteLine(x1);
+Console.WriteLine(y1);
+Console.WriteLine(z1);
+Console.WriteLine(var1);
+Console.WriteLine(var2);
+Console.WriteLine(var3);
+Console.WriteLine(var4);
+Console.WriteLine(var5);
+Console.WriteLine("");
+
+
+//Exercicio 11
+
+float var6 = 345.3456;
+double var7 = 6.89765432127865;
+decimal var8 = 1234567897654300.14567896543;
+
+Console.WriteLine(var6);
+Console.WriteLine(var7);
+Console.WriteLine(var8);
+Console.WriteLine("");
+
+
+//Exercicio 12
+
+char var9 = 'O';
+string var10 = "Tiago Filipe";
+bool var11;
+bool var12;
+
+Console.WriteLine(var9);
+Console.WriteLine(var10);
+
+Console.WriteLine("");
+
+
+//Exercicio 16
+
+double aa = 4.78905;
+double bb = 4.7;
+double cc = 5.8954;
+double dd = 5;
+
+
+
+double roundaa = Math.Round(aa);
+double roundbb = Math.Round(bb);
+double roundcc = Math.Round(cc);
+double rounddd = Math.Round(dd);
+
+
+
+Console.WriteLine(roundaa);
+Console.WriteLine(roundbb);
+Console.WriteLine(roundcc);
+Console.WriteLine(rounddd);
+Console.WriteLine("");
+
+//Exercicio 17
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ Aula 6 7/Aula 6 7/Program.cs
@@ -0,0 +1,115 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Exercicio 1 
+
+int N = 5;
+int i;
+float soma=0;
+
+for (i=0; i<=N; i++)
+{
+    soma = soma + i;
+    
+}
+Console.WriteLine(soma);
+Console.WriteLine("");
+
+//Exercicio 2
+
+int A;
+int B;
+
+Console.WriteLine("Limite inferior: ");
+A = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Limite superior: ");
+B = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("");
+
+for (int x = A; x <= B; x++) 
+
+
+    if (x % 2 == 1)
+        
+    Console.WriteLine(x);
+Console.WriteLine("");
+
+//Exercicio 3
+
+int larg, comp = 0;
+char space = ' ';
+char cross = 'X';
+
+
+
+Console.WriteLine("Largura?");
+larg = Convert.ToInt16((Console.ReadLine()));
+Console.WriteLine("Comprimento?");
+comp = Convert.ToInt16((Console.ReadLine()));
+
+for (int w = 0; w <= larg; w++)
+{
+    Console.Write(cross);
+}
+
+Console.WriteLine();
+
+for (int j = 0; j <= comp; j++)
+{
+    Console.Write(cross);
+
+    for (int k = 0; k <= larg; k++)
+    {
+        Console.Write(space);
+    }
+
+    Console.WriteLine(cross);
+}
+for (int l = 0; l <= larg; l++)
+{
+    Console.Write(cross);
+}
+Console.WriteLine("");
+
+//Exercicio 4
+
+for (int p = 0; p <= 3; p++)
+{
+    Console.WriteLine("OXOXOX");
+    Console.WriteLine("XOXOXO");
+}
+Console.WriteLine();
+
+//Exercicio 5
+
+Console.WriteLine("Numero fatorial desejado?");
+int num = Convert.ToInt16((Console.ReadLine())); ;  
+
+int fatorial = 1; 
+
+for (int n = 1; n <= num; n++)
+{
+    fatorial = fatorial * n;
+}
+Console.WriteLine("Fatorial: " + fatorial); Console.WriteLine();
+
+//Exercicio 6
+
+int numeroAnterior = 0;
+int numeroAtual = 1;
+int fibonacci;
+
+
+for (int q = 0; q <= 20; q++)
+{
+    fibonacci = numeroAnterior + numeroAtual;
+    Console.WriteLine(fibonacci);
+    numeroAnterior = numeroAtual;
+    numeroAtual = fibonacci;
+}
+
+//Exercicio 7
+
+
+
+
